62e9ae7e19930a185a465ff7c91e84b390cd437e
gasperpb <achillespb@hotmail.com>
2022-12-13 11:37:11 -0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,38 @@
-import classes from "./navbar.module.css"
-import React from 'react'
-import logo from "../../assets/logo.jpg"
-import { AiFillHome, AiOutlineCompass, AiOutlineHeart, AiOutlineSearch } from "react-icons/ai"
-import { FiSend } from 'react-icons/fi'
+import React from "react";
+import classes from "./navbar.module.css";
+import logo from "../../assets/logo.jpg";
+
+import {
+    AiOutlineSearch,
+    AiFillHome,
+    AiOutlineCompass,
+    AiOutlineHeart,
+} from "react-icons/ai";
+import { FiSend } from "react-icons/fi";
+
 const Navbar = () => {
     return (
         <div className={classes.container}>
             <div className={classes.wrapper}>
                 <div className={classes.left}>
-                    <img src={logo} className={classes.logo} alt="" />
+                    <img src={logo} alt="" className={classes.logo} />
                 </div>
                 <div className={classes.center}>
-                    <input type="text" className={classes.searchinput} placeholder="Search" />
+                    <input className={classes.searchInput} type="text" placeholder="Search..." />
                     <AiOutlineSearch className={classes.icon} />
                 </div>
                 <div className={classes.right}>
-                    <AiFillHome />
-                    <AiOutlineCompass />
-                    <AiOutlineHeart />
-                    <FiSend />
+                    <AiFillHome className={classes.icon} />
+                    <AiOutlineCompass className={classes.icon} />
+                    <AiOutlineHeart className={classes.icon} />
+                    <FiSend className={classes.icon} />
+                    <div className={classes.profilePicContainer}>
+                        <img src="" alt="" className={classes.profilePicImg} />
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
